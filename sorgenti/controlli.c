dfca7aba848877317e22557789b1f85649671ec6
nicolas bertoncello <nicolas165@Outlook.it>
2023-10-30 15:04:06 +0100

    migliorati: conta_punteggio , controllo mossa illegale

--- sorgenti/controlli.c
+++ sorgenti/controlli.c
@@ -30,16 +30,16 @@ int inserisci_numero_compreso(char* messaggio, int minimo, int massimo) {
   posizione => [1] sx, [2] dx
   piano_gioco => lista tessere all'interno del piano di gioco.
   n_tessere_piano => numero delle tessere presenti nel piano di gioco.*/
-bool mossa_legale(tessera *trovata, int posizione, tessera *piano_gioco, int *n_tessere_piano) {
+bool mossa_legale(tessera *trovata, int posizione, tessera *piano_gioco) {
     /* In base alla posizione, controlla:
         - 1 (sx) --> trovata->estremo_dx = testa->estremo_sx
         - 2 (dx) --> coda->estremo_sx = trovata->estremo_sx
     */
    tessera *posizionata = piano_gioco->successivo;
-    if(posizione == 1 && *n_tessere_piano != 0) {
+    if(posizione == 1 && posizionata != NULL) {
             return posizionata->estremo_sinistro == trovata->estremo_destro ? true : false;
-    } else if(posizione == 2 && *n_tessere_piano != 0) {
-        for(int i = 0; i < *n_tessere_piano; i++){
+    } else if(posizione == 2 && posizionata != NULL) {
+        while(posizionata->successivo != NULL){
             posizionata = posizionata->successivo;
         }
         return posizionata->estremo_destro == trovata->estremo_sinistro ? true : false;
